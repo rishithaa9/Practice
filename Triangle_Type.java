147ed3c5f6fa864e1a9a578f17403fc230e4b13f
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2025-12-04 09:10:33 +0530

    Triangle Type

---
+++ Triangle_Type.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Triangle_Type {
+    public static void main(String args[]){
+        Scanner scanner=new.scanner(System.in);
+
+        l1=scanner.nextInt();
+        l2=scanner.nextInt();
+        l3=scanner.nextInt();
+
+        if()
+    }
+    
+}
