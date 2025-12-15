e2546326563a61573660b1237f55bb0f6ed73a4c
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2025-12-15 09:28:15 +0530

    Power of Four

---
+++ Power_of_four.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        i=0
+        while 4**i<=n:
+            if n== 4**i:
+                return True
+            i+=1
+        return False 
+        
